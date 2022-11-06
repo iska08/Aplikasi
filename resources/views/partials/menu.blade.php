803ceb23a07f0156a50e38971b2568fd72cea0db
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-11-06 10:49:03 +0700

    commit

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -66,24 +66,11 @@
             </li>
             @endcan
             @can('transaction_access')
-            <li class="nav-item">
-                <a href="{{ route("admin.transactions.index") }}"
-                    class="nav-link {{ request()->is('admin/transactions') || request()->is('admin/transactions/*') ? 'active' : '' }}">
-                    <i class="fa-fw fas fa-history nav-icon"></i>
-                    Riwayat Bahan
-                </a>
-            </li>
+            
             @endcan
             @if(file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php')))
             @can('profile_password_edit')
-            <li class="nav-item">
-                <a class="nav-link {{ request()->is('profile/password') || request()->is('profile/password/*') ? 'active' : '' }}"
-                    href="{{ route('profile.password.edit') }}">
-                    <i class="fa-fw fas fa-key nav-icon">
-                    </i>
-                    Ubah Password
-                </a>
-            </li>
+            
             @endcan
             @endif
             <li class="nav-item">
