d6d7d6b1172c5d8746c4e87a3e9809565236859d
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-11-28 09:04:08 +0700

    proyek2

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <ul>
+        <li class="active">Dashboard</li>
+    </ul>
+@endsection
+
+@section('content')
+<!-- Small boxes (Stat box) -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box">
+            <div class="box-body text-center">
+                <h3>Selamat Datang</h3>
+                <h1>
+                    <p>{{ auth()->user()->name }}</p>
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.row (main row) -->
+@endsection
+
+@push('scripts')
+<!-- ChartJS -->
+<script src="{{ asset('AdminLTE-2/bower_components/chart.js/Chart.js') }}"></script>
+<script>
+$(function() {
+    // Get context with jQuery - using jQuery's .get() method.
+    var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
+    // This will get the first returned node in the jQuery collection.
+    var salesChart = new Chart(salesChartCanvas);
+
+    var salesChartData = {
+        labels: {{ json_encode($data_tanggal) }},
+        datasets: [
+            {
+                label: 'Pendapatan',
+                fillColor           : 'rgba(60,141,188,0.9)',
+                strokeColor         : 'rgba(60,141,188,0.8)',
+                pointColor          : '#3b8bba',
+                pointStrokeColor    : 'rgba(60,141,188,1)',
+                pointHighlightFill  : '#fff',
+                pointHighlightStroke: 'rgba(60,141,188,1)',
+                data: {{ json_encode($data_pendapatan) }}
+            }
+        ]
+    };
+
+    var salesChartOptions = {
+        pointDot : false,
+        responsive : true
+    };
+
+    salesChart.Line(salesChartData, salesChartOptions);
+});
+</script>
+@endpush
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,64 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li>
+                <a href="{{ route('dashboard') }}">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+            </li>
+            @if (auth()->user()->level == 1)
+            <li>
+                <a href="{{ route('kategori.index') }}">
+                    <i class="fa fa-cube"></i> <span>Kategori</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('produk.index') }}">
+                    <i class="fa fa-cubes"></i> <span>Produk</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('supplier.index') }}">
+                    <i class="fa fa-truck"></i> <span>Pemasok</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('pengeluaran.index') }}">
+                    <i class="fa fa-money"></i> <span>Pemasukan</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('pembelian.index') }}">
+                    <i class="fa fa-download"></i> <span>Isi Stok</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('penjualan.index') }}">
+                    <i class="fa fa-download"></i> <span>Daftar Permintaan</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('laporan.index') }}">
+                    <i class="fa fa-file-pdf-o"></i> <span>Laporan Pendapatan</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('user.index') }}">
+                    <i class="fa fa-users"></i> <span>Admin Cabang</span>
+                </a>
+            </li>
+            @else
+            <li>
+                <a href="{{ route('transaksi.baru') }}">
+                    <i class="fa fa-cart-arrow-down"></i> <span>Request Barang</span>
+                </a>
+            </li>
+            @endif
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
