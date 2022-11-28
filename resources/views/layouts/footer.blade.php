97617f48974ba31e913e073b66776081ef4db62d
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-11-28 08:58:50 +0700

    proyek2

--- resources/views/layouts/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="main-footer">
-    <!-- <strong>Copyright &copy; {{ date('Y') }} <a href="/">{{ $setting->nama_perusahaan }}</a>.</strong> All rights reserved. -->
-    <strong>{{ date('l, d-m-Y  H:i:s') }}  <a href="/">  {{ $setting->nama_perusahaan }}</a></strong>
-</footer>
